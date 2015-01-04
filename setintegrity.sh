35a342c0ba1df2faaa8d8e87ffbfe2dabd8f1682
Saurabh Agrawal <saurabh.ska@gmail.com>
2015-01-04 01:37:13 -0800

    Script to check and set integrity of all tables in integrity pending state for a DB2 database

---
+++ setintegrity.sh
@@ -0,0 +1,24 @@
+#!/bin/csh
+
+if [[ -n "$2" ]]
+then
+
+	export DB_NAME=$1
+	export DB_SCHEMA=$2
+
+    db2 connect to $DB_NAME
+    db2 set current schema $DB_SCHEMA
+    rm -rf tabname.out
+    db2 -x "select tabname from syscat.tables where tabschema='$DB_SCHEMA' and type='T' and status='C'" | sed -e 's/ //g' > tabname.out
+    while read line
+    do
+        db2 -x "SET INTEGRITY FOR $DB_SCHEMA.$line immediate checked"
+    done < tabname.out
+    db2 connect reset
+    
+else
+    echo ""
+    echo "Usage: setintegrity.sh <DB_NAME> <DB_SCHEMA>"
+    echo ""
+    exit 1
+fi
